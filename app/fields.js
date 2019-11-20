99035e93b7392a97316ef8341d78c29ddef751c8
Roderick Tan <rodericktan@users.noreply.github.com>
2019-11-20 16:33:04 +0000

    Add adult renew filter pages

--- app/fields.js
+++ app/fields.js
@@ -7,5 +7,91 @@ module.exports = {
         items: require('./data/countries'),
         validate: 'required',
         dependent: { field: 'is-uk-application', value: false }
-    }
+    },
+    'date-of-birth': {
+        validate: [
+            // 'required',
+            'date',
+            // { type: 'before' },
+            // { type: 'after', arguments: config.eligibility.after }
+        ],
+        autocomplete: 'bday'
+    },
+    'previous-passport': {
+        formatter: 'boolean',
+        validate: 'required',
+        items: [{
+            value: true
+        }, {
+            value: false
+        }]
+    },
+    'passport-lost': {
+        formatter: 'boolean',
+        validate: 'required',
+        items: [{
+            value: true
+        }, {
+            value: false
+        }]
+    },
+    'passport-issue': {
+        // validate: [
+        //     'required',
+        //     'date',
+        //     {
+        //         type: 'before'
+        //     }, {
+        //         type: 'after',
+        //         arguments: config.eligibility.after
+        //     }, {
+        //         type: 'afterField',
+        //         arguments: ['date-of-birth', true]
+        //     }
+        // ],
+        // autocomplete: 'passport-issue',
+        // offset: 11,
+    },
+    'passport-issuing-authority': {
+        // validate: 'required',
+        items: [
+            'not-other',
+            'other'
+        ],
+    },
+    'damaged': {
+        formatter: 'boolean',
+        // validate: 'required',
+        items: [{
+            value: true
+        }, {
+            value: false
+        }]
+    },
+    'damaged-reason': {
+        validate: [
+            // 'required',
+            'alphanumex1',
+            { type: 'maxlength', arguments: 250 }
+        ],
+        dependent: {
+            field: 'damaged',
+            value: true
+        },
+        // attributes: {
+        //     'spellcheck': 'true',
+        //     'autocapitalize': 'sentences'
+        // }
+    },
+    'other-passports': {
+        formatter: 'boolean',
+        // validate: 'required',
+        items: [{
+            value: true
+        }, {
+            value: false
+        }]
+    },
+
+
 }
