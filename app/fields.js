85f2a8d73fb3b3ab557c57bfe822e70ba30ecabd
Paul Winkler <paul.winkler@digital.homeoffice.gov.uk>
2019-11-21 10:30:49 +0000

    Simpify filter pages

--- app/fields.js
+++ app/fields.js
@@ -9,88 +9,39 @@ module.exports = {
         dependent: { field: 'is-uk-application', value: false }
     },
     'date-of-birth': {
-        validate: [
-            // 'required',
-            'date',
-            // { type: 'before' },
-            // { type: 'after', arguments: config.eligibility.after }
-        ],
+        validate: 'date',
         autocomplete: 'bday'
     },
     'previous-passport': {
         formatter: 'boolean',
-        validate: 'required',
-        items: [{
-            value: true
-        }, {
-            value: false
-        }]
+        validate: 'required'
     },
     'passport-lost': {
         formatter: 'boolean',
-        validate: 'required',
-        items: [{
-            value: true
-        }, {
-            value: false
-        }]
+        validate: 'required'
     },
     'passport-issue': {
-        // validate: [
-        //     'required',
-        //     'date',
-        //     {
-        //         type: 'before'
-        //     }, {
-        //         type: 'after',
-        //         arguments: config.eligibility.after
-        //     }, {
-        //         type: 'afterField',
-        //         arguments: ['date-of-birth', true]
-        //     }
-        // ],
-        // autocomplete: 'passport-issue',
-        // offset: 11,
+        validate: 'date',
+        autocomplete: 'passport-issue',
+        offset: 11
     },
     'passport-issuing-authority': {
-        // validate: 'required',
-        items: [
-            'not-other',
-            'other'
-        ],
     },
     'damaged': {
-        formatter: 'boolean',
-        // validate: 'required',
-        items: [{
-            value: true
-        }, {
-            value: false
-        }]
+        formatter: 'boolean'
     },
     'damaged-reason': {
         validate: [
-            // 'required',
             'alphanumex1',
             { type: 'maxlength', arguments: 250 }
         ],
         dependent: {
             field: 'damaged',
             value: true
-        },
-        // attributes: {
-        //     'spellcheck': 'true',
-        //     'autocapitalize': 'sentences'
-        // }
+        }
     },
     'other-passports': {
-        formatter: 'boolean',
-        // validate: 'required',
-        items: [{
-            value: true
-        }, {
-            value: false
-        }]
+        formatter: 'boolean'
     },
 
 
