0113349f4ee0c3f5d815fc59078dcd8fd0ddf2cf
Roderick Tan <rodericktan@users.noreply.github.com>
2020-02-03 10:01:55 +0000

    Add adult renew documents

---
+++ app/controllers/documents-to-send.js
@@ -0,0 +1,10 @@
+const BaseController = require('./apply')
+
+class DocumentsToSend extends BaseController {
+    get (req, res, next) {
+        res.locals.documentsObject = require('../../app/data/dcs-documents-renew.json')
+        super.get(req, res, next)
+      }
+}
+
+module.exports = DocumentsToSend
