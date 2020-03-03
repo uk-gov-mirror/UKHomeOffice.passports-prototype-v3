d05fa78dd9e29c16d110c2cf1e323ff54a9e9224
Paul Winkler <paul.winkler@digital.homeoffice.gov.uk>
2020-03-03 15:31:28 +0000

    Improve tracking status, links, docs and csig controls

--- app/controllers/track.js
+++ app/controllers/track.js
@@ -11,13 +11,34 @@ class DefaultTrackController extends BaseController {
             resubmissionDeadlineDate: moment(date).add(49, 'days').toISOString()
         })
 
+        // choose documents type
+        let documentsRequired = req.sessionModel.get('documentsRequired') || 'documents'
+        switch (req.query.docs) {
+            case 'documents':
+            case 'passport':
+            case 'none':
+                documentsRequired = req.query.docs.toLowerCase()
+        }
+        req.sessionModel.set({ documentsRequired })
+
+        // choose referee type
+        let csigRequired = req.sessionModel.get('csigRequired')
+        switch (req.query.csig) {
+            case 'true':
+                csigRequired = true
+                break;
+            case 'false':
+                csigRequired = false
+        }
+        req.sessionModel.set({ csigRequired })
+
         // choose tracking status
         let status = req.sessionModel.get('status')
         if (req.query.status) {
             status = req.query.status.toUpperCase()
                 .replace(/[^A-Z]+/g, '_')
         }
-        if (!status) {
+        if (!status || req.form.options.route === '/track/reference') {
             status = req.sessionModel.get('csigRequired')
                 ? 'AWAITING_REFEREE_NOMINATION' : 'SUBMITTED'
         }
@@ -33,10 +54,6 @@ class DefaultTrackController extends BaseController {
             reference,
             formattedReference
         })
-
-        // documents required
-        const documentsRequired = req.sessionModel.get('documentsRequired') || 'documents'
-        req.sessionModel.set({ documentsRequired })
 
         // notifications
         let mobileNotification = req.sessionModel.get('contactPrefsSMS')
