c426ac72ab48158c164e8d86cf73a21d656f5c5e
Vee <v.razazi@kainos.com>
2020-06-01 13:04:23 +0100

    add not eligible for dps page

--- app/steps.js
+++ app/steps.js
@@ -669,6 +669,8 @@ const dps = {
         entryPoint: true,
         next: '/dps/dps-overseas'
     },
+    '/dps/dps-not-eligible':{
+    },
     '/dps/dps-overseas': {
         fields: [
             'isUKApplication',
